bfc4b6ff8bc3f1a106bc30f7319e1dc98d0167f6
Xinran1205 <id19037@bristol.ac.uk>
2025-03-13 01:01:38 +0000

    practice xxljob

---
+++ xxljobPractice/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+// 很遗憾，这个项目我始终跑不起来！！！
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
